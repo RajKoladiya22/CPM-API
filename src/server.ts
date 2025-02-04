9f4b089a57cc548d456e9ce7c21c14f9c39b98cc
RajKoadiya22 <kolladiyaraj22@gmail.com>
2025-02-04 17:58:19 +0530

    index

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "http://localhost:5173", // Allow requests only from this origin
+  origin: "https://cpm-api.vercel.app", // Allow requests only from this origin
   methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"] // Allow specific headers
 }));
